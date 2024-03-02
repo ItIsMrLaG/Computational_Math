09d74098e21db6afefe775593ac611cb5f32055a
Georgy S <it.is.mr.lag@gmail.com>
2024-03-02 17:24:59 +0300

    feat: MVP

--- task1/src/main.c
+++ task1/src/main.c
@@ -1,19 +1,11 @@
-
-#include <iso646.h>
 #include <math.h>
 #include <omp.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "main.h"
 
-/*
-Setting the number of threads:
-[ export OMP_NUM_THREADS=8 ]
-
-h := size/(N+1)
-*/
+#define BLOCK_SIZE 64
 
 #define x_ij(i, j, pb) (i * pb->h + pb->shift.x)
 #define y_ij(i, j, pb) (i * pb->h + pb->shift.y)
@@ -21,18 +13,20 @@ h := size/(N+1)
 #define b_k0(bk, bl_size) (bk * bl_size + 1)
 #define b_kn(k0, bl_size, pb) min(k0 + bl_size, pb->size - 1)
 
-static inline double **_alloc_u(size_t N) {
+// #define DEBUG
+
+static double **my_alloc_u(int64_t N) {
   double **u = calloc(N, sizeof(double *));
 
   if (u == NULL)
     return NULL;
 
-  for (size_t i = 0; i <= N; i++) {
+  for (int64_t i = 0; i < N; i++) {
     u[i] = calloc(N, sizeof(double));
 
     if (u[i] == NULL) {
 
-      for (size_t j = 0; j < i; j++)
+      for (int64_t j = 0; j < i; j++)
         free(u[i]);
 
       free(u);
@@ -42,18 +36,20 @@ static inline double **_alloc_u(size_t N) {
   return u;
 }
 
-static inline void _free_u(size_t N, double **u) {
-  for (size_t i = 0; i <= N; i++)
+static inline void my_free_u(int64_t N, double **u) {
+  for (int64_t i = 0; i < N; i++)
     free(u[i]);
   free(u);
 }
 
-static inline void init_matrix(pb_parms *pb, func f, func d2f) {
+static inline void init_matrix(pb_parms *pb, func f, func u) {
 
-  for (size_t i = 0; i <= pb->size; i++) {
-    for (size_t j = 0; j <= pb->size; j++) {
+  int64_t i, j;
+
+  for (i = 0; i < pb->size; i++) {
+    for (j = 0; j < pb->size; j++) {
       if ((i == 0) || (j == 0) || (i == pb->size) || (j == pb->size)) {
-        pb->u[i][j] = d2f(x_ij(i, j, pb), y_ij(i, j, pb));
+        pb->u[i][j] = u(x_ij(i, j, pb), y_ij(i, j, pb));
       } else {
         pb->u[i][j] = 0;
       }
@@ -61,40 +57,56 @@ static inline void init_matrix(pb_parms *pb, func f, func d2f) {
       pb->f[i][j] = f(x_ij(i, j, pb), y_ij(i, j, pb));
     }
   }
+
+#ifdef DEBUG
+  for (size_t i = 0; i <= pb->size; i++) {
+    printf("\n %lu: ", i);
+    for (size_t j = 0; j <= pb->size; j++) {
+      printf("|(%lu) %f ", j, pb->u[i][j]);
+    }
+  }
+#endif
 }
 
-static inline pb_parms *create_pb(size_t N, double grid_size, double eps,
+static inline pb_parms *create_pb(int64_t N, double grid_size, double eps,
                                   point l_down) {
   pb_parms *pb = calloc(1, sizeof(pb_parms));
 
   if (pb == NULL)
     return pb;
 
-  pb->size = N + 1;
+  pb->size = N + 2;
   pb->grid_size = grid_size;
   pb->eps = eps;
   pb->h = grid_size / pb->size;
   pb->shift = l_down;
 
-  pb->u = _alloc_u(pb->size);
-  pb->f = _alloc_u(pb->size);
+  pb->u = my_alloc_u(pb->size);
+  pb->f = my_alloc_u(pb->size);
 
   if (pb->u == NULL || pb->f == NULL)
     return NULL;
-
+#ifdef DEBUG
+  for (size_t i = 0; i <= pb->size; i++) {
+    printf("\n %lu: ", i);
+    for (size_t j = 0; j <= pb->size; j++) {
+      printf("|(%lu) %f ", j, pb->u[i][j]);
+    }
+  }
+#endif
   return pb;
 }
 
-static inline double process_block(pb_parms *pb, size_t bx, size_t by,
-                                   size_t bl_size) {
-  size_t i0 = b_k0(bx, bl_size);
-  size_t in = b_kn(i0, bl_size, pb);
-  size_t j0 = b_k0(by, bl_size);
-  size_t jn = b_kn(j0, bl_size, pb);
+static double process_block(pb_parms *pb, int64_t bx, int64_t by,
+                            int64_t bl_size) {
+  int64_t i0 = b_k0(bx, bl_size);
+  int64_t in = b_kn(i0, bl_size, pb);
+  int64_t j0 = b_k0(by, bl_size);
+  int64_t jn = b_kn(j0, bl_size, pb);
   double dm = 0;
 
-  for (size_t i = i0; i < in; i++) {
-    for (size_t j = j0; j < jn; j++) {
+  for (int64_t i = i0; i < in; i++) {
+    for (int64_t j = j0; j < jn; j++) {
       double temp = pb->u[i][j];
 
       pb->u[i][j] = u_ij(i, j, pb);
@@ -108,22 +120,121 @@ static inline double process_block(pb_parms *pb, size_t bx, size_t by,
   return dm;
 }
 
-pb_parms *approximate_values(uint32_t N, double grid_size, double eps,
-                             point l_down, func f, func d2f) {
+void free_pb(pb_parms *pb) {
+  if (pb == NULL) {
+    return;
+  }
 
+  //	TODO:
+  //  my_free_u(pb->size, pb->u);
+  //  my_free_u(pb->size, pb->f);
+  //  free(pb);
+}
+
+int64_t process_blocks(pb_parms *pb) {
+  double dmax;
+  int64_t iter = 0;
+  int64_t bl_size = BLOCK_SIZE;
+  int64_t N, NB;
+  N = pb->size - 2;
+  NB = N / BLOCK_SIZE;
+
+  if (N < BLOCK_SIZE)
+    return -MEM_ERR;
+  if (NB * BLOCK_SIZE < N)
+    NB += 1;
+
+  double *dm = calloc(NB, sizeof(double));
+  // double dm[3] = {0};
+
+  double **un = my_alloc_u(N);
+  if (dm == NULL || un == NULL)
+    return -MEM_ERR;
+
+  do {
+    int64_t bj, bi;
+    int64_t nx;
+    double res;
+    iter++;
+    dmax = 0;
+
+    for (nx = 0; nx < NB; nx++) {
+      dm[nx] = 0;
+
+// #pragma omp parallel for shared(pb, nx, dm) private(bi, bj, res)
+      for (bi = 0; bi < nx + 1; bi++) {
+
+        bj = nx - bi;
+
+        res = process_block(pb, bi, bj, bl_size);
+
+        if (dm[bi] < res)
+          dm[bi] = res;
+      }
+    }
+
+    for (nx = NB - 2; nx >= 0; nx--) {
+
+// #pragma omp parallel for shared(pb, nx, dm) private(bi, bj, res)
+      for (bi = 0; bi < nx + 1; bi++) {
+        bj = 2 * (NB - 1) - nx - bi;
+        res = process_block(pb, bi, bj, bl_size);
+        if (dm[bi] < res)
+          dm[bi] = res;
+      }
+    }
+
+    for (int64_t k = 0; k < NB; k++) {
+      if (dmax < dm[k])
+        dmax = dm[k];
+    }
+    printf("%f\n", dmax);
+  } while (dmax > pb->eps);
+  free(dm);
+  // free(um???)
+
+  return iter;
+}
+
+pb_parms *approximate_values(int64_t N, double grid_size, double eps,
+                             point l_down, func f, func u) {
   pb_parms *pb = create_pb(N, grid_size, eps, l_down);
-  double **un = _alloc_u(N);
-  size_t iter = 0;
+  int64_t iter = 0;
 
-  if (pb == NULL || un == NULL)
+  if (pb == NULL)
     return NULL;
-  init_matrix(pb, f, d2f);
 
-  return NULL;
+  init_matrix(pb, f, u);
+  iter = process_blocks(pb);
+  if (iter < SUCCESS) {
+    free_pb(pb);
+    return NULL;
+  }
+  printf("res: %lu !", iter);
+  return pb;
 }
 
-void free_pb(pb_parms *) {
-  // TODO:
+// double d_x3_p_y3(double x, double y) { return 6 * x + 6 * y; }
+
+// double x3_p_y3(double x, double y) { return pow(x, 3) + pow(y, 3); }
+
+double d_kx3_p_2ky3(double x, double y) { return 6000 * x + 12000 * y; }
+
+double kx3_p_2ky3(double x, double y) {
+  return 1000 * pow(x, 3) + 2000 * pow(y, 3);
 }
 
-int main(int argc, char *argv[]) { return 0; }
+int main(void) {
+
+  omp_set_num_threads(8);
+  func f = d_kx3_p_2ky3; // d_book;q
+  func u = kx3_p_2ky3;   // book;
+  point p = {0};
+
+  // int64_t sz[] = {100, 200, 300, 500, 1000, 2000, 3000};
+  pb_parms *res = approximate_values(1000, 1, 0.1, p, f, u);
+  free_pb(res);
+  if (res)
+    printf("good");
+  return 0;
+}
